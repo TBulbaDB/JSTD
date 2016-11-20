33c517454f41d0b0a2536886d2365cbd29eacb13
Taras Bulba <tbulba.db@gmail.com>
2016-11-20 17:28:45 +0300

    Project

---
+++ JSTD.Web/wwwroot/js/jstd.js
@@ -0,0 +1,23 @@
+﻿var grid = function (width, height) {
+    return new Array(width, height);
+}
+
+var gridTests = function (qUnit) {
+    //var testConsole = $("#test-console");
+    //function writeLine(value) {
+    //    testConsole.appendChild("<div>" + value + "</div>");
+    //}
+
+    function testCreateGrid() {
+        qUnit.test("create grid", function (assert) {
+            var testGrid = grid(10, 10);
+            assert.ok(1 === "1", "Passed!");
+        });
+    }
+
+    return {
+        TestAll: function() {
+            testCreateGrid();
+        }
+    }
+}(window.QUnit);
